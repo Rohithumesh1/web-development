5b9bcad771647e4b94f3c300931e4b8a279db95d
Pratik K <undopatika@gmail.com>
2023-01-29 17:03:17 +0530

    Video carousel added

--- index.js
+++ index.js
@@ -1,46 +1,17 @@
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-container');
-let formBtn = document.querySelector('#login-btn');
-let loginForm = document.querySelector('.login-form-container');
-let formClose = document.querySelector('#form-close');
-let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let videoBtn = document.querySelectorAll('.vid-btn');
+document.querySelector('#video-slider').src = "videos/vid-1.mp4";
 
-window.onscroll = () => {
-  searchBtn.classList.remove('fa-times');
-  searchBar.classList.remove('active');
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
-  loginForm.classList.remove('active');
+const videoChanger = (item, index) => {
+  item.addEventListener("click", () => { 
+    document.querySelector('.controls .active').classList.remove('active');
+    item.classList.add('active');
+    let src = item.getAttribute('data-src');
+    document.querySelector('#video-slider').src = src;
+   })
 }
 
-menu.addEventListener('click', () => {
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-});
-
-searchBtn.addEventListener('click', () => {
-  searchBtn.classList.toggle('fa-times');
-  searchBar.classList.toggle('active');
-});
-
-formBtn.addEventListener('click', () => {
-  loginForm.classList.add('active');
-});
-
-formClose.addEventListener('click', () => {
-  loginForm.classList.remove('active');
-});
-
-videoBtn.forEach(btn => {
-  btn.addEventListener('click', () => {
-    document.querySelector('.controls .active').classList.remove('active');
-    btn.classList.add('active');
-    let src = btn.getAttribute('data-src');
-    document.querySelector('#video-slider').src = src;
-  });
-});
+videoBtn.forEach(videoChanger);
 
 var swiper = new Swiper(".review-slider", {
   spaceBetween: 20,
