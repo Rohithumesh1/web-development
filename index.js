6b93312fe00070f054729c3f9856ea6cf2b40203
Pratik K <undopatika@gmail.com>
2023-01-29 14:40:49 +0530

    Organize and clean up
    Separated according to the pages and linked all the images hierarchically.

--- index.js
+++ index.js
@@ -7,80 +7,80 @@ let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let videoBtn = document.querySelectorAll('.vid-btn');
 
-window.onscroll = () =>{
-    searchBtn.classList.remove('fa-times');
-    searchBar.classList.remove('active');
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-    loginForm.classList.remove('active');
+window.onscroll = () => {
+  searchBtn.classList.remove('fa-times');
+  searchBar.classList.remove('active');
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  loginForm.classList.remove('active');
 }
 
-menu.addEventListener('click', () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+menu.addEventListener('click', () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
 });
 
-searchBtn.addEventListener('click', () =>{
-    searchBtn.classList.toggle('fa-times');
-    searchBar.classList.toggle('active');
+searchBtn.addEventListener('click', () => {
+  searchBtn.classList.toggle('fa-times');
+  searchBar.classList.toggle('active');
 });
 
-formBtn.addEventListener('click', () =>{
-    loginForm.classList.add('active');
+formBtn.addEventListener('click', () => {
+  loginForm.classList.add('active');
 });
 
-formClose.addEventListener('click', () =>{
-    loginForm.classList.remove('active');
+formClose.addEventListener('click', () => {
+  loginForm.classList.remove('active');
 });
 
-videoBtn.forEach(btn =>{
-    btn.addEventListener('click', ()=>{
-        document.querySelector('.controls .active').classList.remove('active');
-        btn.classList.add('active');
-        let src = btn.getAttribute('data-src');
-        document.querySelector('#video-slider').src = src;
-    });
+videoBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.querySelector('.controls .active').classList.remove('active');
+    btn.classList.add('active');
+    let src = btn.getAttribute('data-src');
+    document.querySelector('#video-slider').src = src;
+  });
 });
 
 var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
     },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
+    768: {
+      slidesPerView: 2,
     },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
 
 var swiper = new Swiper(".brand-slider", {
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-        450: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        991: {
-          slidesPerView: 4,
-        },
-        1200: {
-          slidesPerView: 5,
-        },
-      },
+    768: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
 }); 
