7520e187303eba00f8c745470785fc531dc80d4f
Pratik K <undopatika@gmail.com>
2023-04-05 01:53:41 +0530

    Final Authentication done and favicon added

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+// Document elements from index and all other pages
 let videoBtn = document.querySelectorAll('.vid-btn');
 let loginBtn = document.querySelector("#login");
 let logoutBtn = document.querySelector("#logout");
 let loginFormCont = document.querySelector(".login-form-container");
 let loginNowBtn = document.querySelector(".login-form-container .btn");
 let navbar = document.querySelector(".navbar");
+let userError = document.querySelector(".user-not-found");
+let textEmail = document.getElementById("email");
+let textPassword = document.getElementById("password");
 
-const credentials = [{ username: "rohith@travelup.com", password: "1qazmlp0", img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Infinite.svg/640px-Infinite.svg.png" }];
+// Other variables and constants
+const credentials = [
+  {
+    username: "rohith@travelup.com",
+    password: "1qazmlp0",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Infinite.svg/640px-Infinite.svg.png"
+  },
+  {
+    username: "pratik@travelup.com",
+    password: "fdhjklmno18",
+    img: "https://avatars.githubusercontent.com/u/66823602?v=4"
+  },
+  {
+    username: "qwer",
+    password: "123"
+  }
+];
 
+// Each "vid-btn" under "control" class has toggle(onClick) functionality for changing the video source
 const videoChanger = (item, index) => {
   item.addEventListener("click", () => {
     document.querySelector('.controls .active').classList.remove('active');
@@ -16,7 +37,8 @@ const videoChanger = (item, index) => {
   })
 };
 
-const toggleLogin = () => {
+// Toggle the login Form up and down
+const toggleLoginForm = () => {
   if (loginFormCont.className === "login-form-container") {
     loginFormCont.className = "login-form-container active";
   } else if (loginFormCont.className === "login-form-container active") {
@@ -24,45 +46,25 @@ const toggleLogin = () => {
   }
 };
 
+// Toggle the logout button
 const toggleLogout = () => {
   localStorage.clear();
   window.location.href = "https://rohithumesh1.github.io/web-development/";
   window.location.reload();
 };
 
-function setCookie(name, value, daysToLive) {
-  const date = new Date();
-  date.setTime(date.getTime() + (daysToLive * 24 * 60 * 60 * 1000));
-  let expires = "expires=" + date.toUTCString();
-  document.cookie = `${name}=${value}; ${expires}; path=/`
-}
-function deleteCookie(name) {
-  setCookie(name, null, null);
-}
-function getCookie(name) {
-  const cDecoded = decodeURIComponent(document.cookie);
-  const cArray = cDecoded.split("; ");
-  let result = null;
-
-  cArray.forEach(element => {
-    if (element.indexOf(name) == 0) {
-      result = element.substring(name.length + 1)
-    }
-  })
-  return result;
-}
-
-const checkLoggedIn = () => { 
-  if(localStorage.getItem("username")){
+// Checking "loged in" if local storage contains any username(email) 
+const checkLoggedIn = () => {
+  if (localStorage.getItem("username")) {
     loginBtn.style.display = 'none';
     navbar.style.display = '';
-    console.log("Logged in");
-  }else{
+  } else {
     logoutBtn.style.display = 'none';
     navbar.style.display = 'none';
   }
 };
 
+// Setting user credentials in local storage for authentication
 const setLoggedIn = (username, password, image) => {
   localStorage.setItem("username", username);
   localStorage.setItem("password", password);
@@ -70,21 +72,30 @@ const setLoggedIn = (username, password, image) => {
 
   loginBtn.style.display = 'none';
   logoutBtn.style.display = "";
-  loginFormCont.className = "login-form-container";
   window.location.reload();
 };
 
-const checkCredentials = () => { };
+// Checking email & password from input to credentials variable
+const checkCredentials = (email, pass) => {
+  credentials.forEach((item, index) => {
+    if (email === item.username && pass === item.password) {
+      setLoggedIn(item.username, item.password, item.img);
+      return;
+    }
+  });
+};
 
+
+// Actual functionality begins from here
 
 checkLoggedIn();
 
+// for each button its going to call the function "videoChanger"
 videoBtn.forEach(videoChanger);
 
 
-
-loginNowBtn.addEventListener('click', () => { 
-  setLoggedIn("lowda", "12345", "tfyuvhjbjihgyfchgcguj");
+// On clicking login button, authentication will begin
+loginNowBtn.addEventListener('click', () => {
+  checkCredentials(textEmail.value, textPassword.value);
+  textEmail.value = textPassword.value = "";
 });
-
-
